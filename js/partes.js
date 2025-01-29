64746c4ff5cdde2254d36a8c4c455cf8b355de3f
Jorge <jrncarrizo@gmail.com>
2025-01-29 20:56:29 -0300

    ultima

--- js/partes.js
+++ js/partes.js
@@ -60,3 +60,53 @@ carousel.addEventListener('slide.bs.carousel', (event) => {
     }
   });
 });
+
+// tamaño de foto al clikear
+function openModal(img) {
+    const modal = document.getElementById("imageModal");
+    const modalImg = document.getElementById("modalImage");
+    const captionText = document.getElementById("caption");
+
+    modal.style.display = "block";
+    modalImg.src = img.src; // Establece la fuente de la imagen modal
+    captionText.innerHTML = img.alt; // Agrega el texto alternativo como título
+}
+
+function closeModal() {
+    const modal = document.getElementById("imageModal");
+    modal.style.display = "none"; // Oculta el modal
+}
+
+
+// Mail
+
+var form = document.getElementById("my-form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("my-form-status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          status.innerHTML = "Thanks for your submission!";
+          form.reset()
+        } else {
+          response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            } else {
+              status.innerHTML = "Oops! There was a problem submitting your form"
+            }
+          })
+        }
+      }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
